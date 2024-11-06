c6d52cae3e0b713304c13029d28be9a65b7f6bbb
dahsur <yogenv@gmail.com>
2024-11-06 14:13:12 +0100

    removed key

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -7,7 +7,7 @@ from langchain_community.embeddings import OpenAIEmbeddings
 
 def get_embedding_function():
     # Use OpenAI's text-embedding-ada-002 model for embeddings
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key="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")
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key="xxxxxxxxxxxx")
     return embeddings
 
 #def get_embedding_function():
